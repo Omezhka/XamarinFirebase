d09f21b602a778794e28048c7b7acce711cd6ce5
Наталья Аверкова <falabellla-61@mail.ru>
2020-06-14 18:34:01 +0300

    Retrieving data from Firebase

---
+++ XamarinFirebase/EventListeners/DrugsListeners.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Database;
+using XamarinFirebase.Data_Models;
+using XamarinFirebase.Helpers;
+
+namespace XamarinFirebase.EventListeners
+{
+    public class DrugsListeners : Java.Lang.Object, IValueEventListener
+    {
+
+        List<Drugs> drugsList = new List<Drugs>();
+
+        public event EventHandler<DrugsDataEventArgs> DrugsRetrived;
+
+        public class DrugsDataEventArgs : EventArgs
+        {
+            public List<Drugs> Drugs { get; set; }
+        }
+
+        public void OnCancelled(DatabaseError error)
+        {
+            
+        }
+
+        public void OnDataChange(DataSnapshot snapshot)
+        {
+           if(snapshot.Value != null)
+            {
+                var child = snapshot.Children.ToEnumerable<DataSnapshot>();
+                drugsList.Clear();
+                foreach (DataSnapshot drugsData in child)
+                {
+                    Drugs drug = new Drugs();
+                    drug.ID = drugsData.Key;
+                    drug.Name = drugsData.Child("fullname").Value.ToString();
+                    drug.ActiveSubstance = drugsData.Child("activeSubstance").Value.ToString();
+                    drug.Form = drugsData.Child("form").Value.ToString();
+                    drug.Group = drugsData.Child("group").Value.ToString();
+
+                    drugsList.Add(drug);
+                }
+                DrugsRetrived.Invoke(this, new DrugsDataEventArgs { Drugs = drugsList });
+            }
+        }
+
+        public void Create()
+        {
+            DatabaseReference drugsRef = AppDataHelper.GetDatabase().GetReference("drug");
+            drugsRef.AddValueEventListener(this);
+        }
+    }
+}
--- XamarinFirebase/MainActivity.cs
+++ XamarinFirebase/MainActivity.cs
@@ -9,6 +9,7 @@ using XamarinFirebase.Adapter;
 using System.Collections.Generic;
 using XamarinFirebase.Data_Models;
 using XamarinFirebase.Fragments;
+using XamarinFirebase.EventListeners;
 
 namespace XamarinFirebase
 {
@@ -20,6 +21,9 @@ namespace XamarinFirebase
         EditText searchText;
         RecyclerView myRecycleView;
         List<Drugs> DrugsList;
+
+        DrugsListeners drugsListeners;
+
         AddDrugFragment addDrugFragment;
 
 
@@ -38,10 +42,11 @@ namespace XamarinFirebase
             searchButton.Click += SearchButton_Click;
 
             addButton.Click += AddButton_Click;
-            CreateData();
+            RetriveData();
+            //CreateData();
 
-            SetupRecycleView();
-        }
+           // SetupRecycleView();
+        } 
 
         private void AddButton_Click(object sender, EventArgs e)
         {
@@ -57,16 +62,29 @@ namespace XamarinFirebase
             myRecycleView.SetAdapter(adapter);
         }
 
-        public void CreateData()
+        //public void CreateData()
+        //{
+        //    DrugsList = new List<Drugs>();
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 111", Name = "Name 111", ID = "1", Form = "2011", Group = "Status 111" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 222", Name = "Name 222", ID = "2", Form = "2012", Group = "Status 222" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 333", Name = "Name 333", ID = "3", Form = "2013", Group = "Status 333" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 444", Name = "Name 444", ID = "4", Form = "2014", Group = "Status 444" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 555", Name = "Name 555", ID = "5", Form = "2015", Group = "Status 555" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 666", Name = "Name 666", ID = "6", Form = "2016", Group = "Status 666" });
+        //    DrugsList.Add(new Drugs { ActiveSubstance = "Department 666", Name = "Name 777", ID = "7", Form = "2017", Group = "Status 777" });
+        //}
+
+        public void RetriveData()
         {
-            DrugsList = new List<Drugs>();
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 111", Name = "Name 111", ID = "1", Form = "2011", Group = "Status 111" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 222", Name = "Name 222", ID = "2", Form = "2012", Group = "Status 222" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 333", Name = "Name 333", ID = "3", Form = "2013", Group = "Status 333" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 444", Name = "Name 444", ID = "4", Form = "2014", Group = "Status 444" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 555", Name = "Name 555", ID = "5", Form = "2015", Group = "Status 555" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 666", Name = "Name 666", ID = "6", Form = "2016", Group = "Status 666" });
-            DrugsList.Add(new Drugs { ActiveSubstance = "Department 666", Name = "Name 777", ID = "7", Form = "2017", Group = "Status 777" });
+            drugsListeners = new DrugsListeners();
+            drugsListeners.Create();
+            drugsListeners.DrugsRetrived += DrugsListeners_DrugsRetrived;
+        }
+
+        private void DrugsListeners_DrugsRetrived(object sender, DrugsListeners.DrugsDataEventArgs e)
+        {
+            DrugsList = e.Drugs;
+            SetupRecycleView();
         }
 
         private void SearchButton_Click(object sender, System.EventArgs e)
